d025e4b877a09a67c1c6b5e91789f603c381d52a
AbianSL <alu0101487458@ull.edu.es>
2022-09-05 19:05:14 +0100

    starting the basic structure of a peice

--- src/pieces.h
+++ src/pieces.h
@@ -1,11 +1,40 @@
 #ifndef PIECES_H
 #define PIECES_H 
 
-class king {
- public:
+#include <vector>
+#include <pair_t.h>
 
+enum Colour {
+  white,
+  black,
+};
+
+enum NamePieces {
+  king,
+  queen,
+  pawn,
+};
+
+struct PiecesHealthy{
+  std::vector<pair_t<int>> white_pieces_healthy;
+  std::vector<pair_t<int>> black_pieces_healthy;
+};
+
+class King {
+ public:
+  King(const Colour& kCoulor, const NamePieces kName) {
+    colour_king_ = kCoulor;
+    if (kCoulor == white) {
+      white_pieces_healthy.
+    } else if () {
+
+    } else {
+
+    }
+  }
  private:
-  
+  Colour colour_king_;
+  const NamePieces name_king {king};
 };
 
 #endif PIECES_H
