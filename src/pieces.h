bfac94667062692c30a4519eafc0cc6002424184
AbianSL <alu0101487458@ull.edu.es>
2022-09-07 21:27:49 +0100

    put the state in the vector

--- src/pieces.h
+++ src/pieces.h
@@ -3,47 +3,45 @@
 
 #include <vector>
 namespace Characteristics {
+  /**
+   * @brief the list of colour that exist
+   */
   enum Colour {
     white,
     black,
   };
 
+  /**
+   * @brief name of all piece in chess
+   */
   enum NamePieces {
     king,
     queen,
     pawn,
   };
 
+  /**
+   * @brief the structure basic to know if the piece exist and the name of it
+   */
   struct Pair{
     NamePieces name;
     bool alive;
   };
 }
 
-/**
- * @brief that struct is for clasificate if the pieces are death or alive
- * 
- */
-struct PiecesHealthy{
-  std::vector<Characteristics::Pair> white_pieces_healthy;
-  std::vector<Characteristics::Pair> black_pieces_healthy;
-};
+// that global vector is to store the white and black pieces that live
+std::vector<Characteristics::Pair> white_pieces_healthy;
+std::vector<Characteristics::Pair> black_pieces_healthy;
 
-class King : PiecesHealthy{
+void Constructor(const Characteristics::Colour& kCoulor, const Characteristics::NamePieces& kName);
+
+class King {
  public:
-  King(const Characteristics::Colour& kCoulor, const Characteristics::NamePieces& kName) {
-    colour_king_ = kCoulor;
-    if (kCoulor == Characteristics::white) {
-
-    } else if (kCoulor == Characteristics::black) {
-      white_pieces_healthy.push_back();
-    } else {
-
-    }
-  }
+  King(const Characteristics::Colour& kCoulor, Characteristics::NamePieces& name);
  private:
   Characteristics::Colour colour_king_;
   const Characteristics::NamePieces name_king {Characteristics::king};
 };
+
 
 #endif PIECES_H
