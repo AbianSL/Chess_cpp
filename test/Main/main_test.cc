2631b9de3f70ac8f559e290d505a25a98e2a0e10
AbianSL <alu0101487458@ull.edu.es>
2023-01-31 19:06:09 +0000

    starting basic of the main test

---
+++ test/Main/main_test.cc
@@ -0,0 +1,19 @@
+/**
+ * @file main_test.cc
+ * @author Abian Santana Ledesma
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+GTEST_API_ int main(int argc, char **argv) {
+        std::cout << "Executing main() from program gtest_main.cc\n";
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_A;
+}
