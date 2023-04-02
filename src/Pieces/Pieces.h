b4918a89962a3b73999ace75910fdcce942447e7
AbianSL <abisanle@gmail.com>
2023-04-02 16:31:04 +0100

    first try to make the piece class

---
+++ src/Pieces/Pieces.h
@@ -0,0 +1,28 @@
+/**
+ * @file Pieces.h
+ * @author AbianSL
+ * @brief That File declare the generic class "Piece"
+ * @version 0.1
+ * @date 2023-04-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <string> // std::string
+#include <cstdint> // int8_t
+
+class Piece {
+ public:
+  virtual std::string GetPieceName() const = 0;
+  virtual unsigned char GetPieceColor() const = 0;
+
+  virtual bool IsBeingAttacked() const = 0;
+  virtual bool IsBeingMoved() const = 0;
+
+  virtual bool Attack() const = 0;
+  virtual bool Move() const = 0;
+ private:
+  uint8_t piece_color; // 0 = white, 1 = black
+  std::string piece_name;
+};
