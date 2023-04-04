dbf2ed580ba3164b61c2b0c50b4ae8f1293d5dde
AbianSL <abisanle@gmail.com>
2023-04-04 01:20:03 +0100

    all method definited

---
+++ src/Pieces/Pieces_CC/King.cc
@@ -0,0 +1,53 @@
+/**
+ * @file King.cc
+ * @author AbianSL (you@domain.com)
+ * @brief Definition of the class "King"
+ * @version 0.1
+ * @date 2023-04-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "King.h"
+
+/**
+ * @brief Construct a new King:: King object
+ * 
+ * @param piece_color That parameter determinate the 
+ *                    color of the piece
+ */
+King::King(uint8_t piece_color) {
+  piece_color_ = piece_color;
+  piece_name_ = "King";
+}
+
+/**
+ * @brief Get the Piece Name object
+ * 
+ * @return std::string the name of the piece 
+ */
+std::string King::GetPieceName() const {
+  return piece_name_;
+}
+
+/**
+ * @brief Get the Piece Color object
+ * 
+ * @return unsigned char the color of the piece
+ *
+ * @note 0 = white, 1 = black
+ */
+unsigned char King::GetPieceColor() const {
+  return piece_color_;
+}
+
+/**
+ * @brief Check if the piece is being attacked
+ * 
+ * @return true if the piece is being attacked
+ * @return false if the piece is not being attacked
+ */
+bool King::IsBeingAttacked() const {
+  return false;
+}
