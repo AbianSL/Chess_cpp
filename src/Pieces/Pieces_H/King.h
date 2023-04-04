1166e13e0649519c28dc90d57aa87c67035d7be8
AbianSL <abisanle@gmail.com>
2023-04-04 01:19:43 +0100

    declaratation of the king class

---
+++ src/Pieces/Pieces_H/King.h
@@ -0,0 +1,25 @@
+/**
+ * @file King.h
+ * @author AbianSL (you@domain.com)
+ * @brief Declaration of the class "King"
+ * @version 0.1
+ * @date 2023-04-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef KING_H
+#define KING_H
+
+#include "../Pieces.h"
+
+class King : protected Piece {
+ public:
+  King(uint8_t piece_color);
+
+  std::string GetPieceName() const override;
+  unsigned char GetPieceColor() const override;
+};
+
+#endif  // KING_H
